6b36e2fc334e1ea179fabacaa9db57400e4dbcfe
Bill Carlo Vergara <b.vergara>
2021-04-16 07:22:58 +0800

    quicksort answer

--- AlgoKata/src/Basics/QuickSort.java
+++ AlgoKata/src/Basics/QuickSort.java
@@ -2,9 +2,6 @@ package Basics;
 
 import Utils.Utils;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class QuickSort {
     public static void main(String[] args) throws Exception {
         for(int t=0 ; t<100 ; t++){
@@ -18,12 +15,40 @@ public class QuickSort {
                 int prev = arr[i-1];
                 int next = arr[i];
                 Utils.expect(prev <= next, "wrong sort");
+                Utils.printArr(arr);
             }
         }
     }
 
     static int[] quickSort(int[] arr){
-        // insertCode
+        quickSort(arr, 0, arr.length);
         return arr;
+    }
+
+    private static void quickSort(int[] arr, int startI, int endI) {
+        if(endI - startI <= 1) return;
+        int divI = divide(arr, startI, endI);
+        quickSort(arr, startI, divI);
+        quickSort(arr, divI, endI);
+    }
+
+    private static int divide(int[] arr, int startI, int endI) {
+        int chosenPos = startI;
+        int chosen = arr[endI - 1];
+        for(int i=startI ; i<endI-1 ; i++){
+            int curr = arr[i];
+            if(curr <= chosen){
+                swap(arr, chosenPos, i);
+                chosenPos++;
+            }
+        }
+        swap(arr, chosenPos, endI-1);
+        return chosenPos;
+    }
+
+    static void swap(int[] arr, int i1, int i2){
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
     }
 }
